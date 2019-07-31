28d8d9917f7defd3af4834d53bb6466694c998b8
Kriviears <kriviears@gmail.com>
2019-07-31 18:29:06 +0000

    Initial commit

---
+++ test/app.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+'use strict';
+
+
+const app = require('../src/app');
+
+describe('App', ()=>{
+  it('GET / responds with 200 containing "Hello boilerplate!"',()=>{
+    return supertest(app)
+      .get('/')
+      .expect(200, 'Hello boilerplate!');
+  });
+});
